62f716cde90028c20ba63c8ffdcfef7ab8385495
半山Alec <627649674@qq.com>
2024-09-26 21:49:12 +0800

    feat: 支持react自定义四个角的圆角

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
 import { Circle, Rect, Text, Ellipse, Line } from './simple';
 
-declare global {
-  interface Window {
-    CanvasElements: any;
-  }
-}
-
-window.CanvasElements = {
-  Circle,
-  Rect,
-  Text,
-  Ellipse,
-  Line,
-} || {};
-
-
 export { Circle, Rect, Text, Ellipse, Line };
--- src/simple/rect.ts
+++ src/simple/rect.ts
@@ -2,7 +2,7 @@ import { Element } from './base';
 
 export default class Rect extends Element {
 
-  r: number;
+  r: number | [number, number, number, number];
   w: number;
   h: number;
 
@@ -13,7 +13,7 @@ export default class Rect extends Element {
    * @param {number} options.y y coordinate
    * @param {number} options.w rectangle width
    * @param {number} options.h rectangle height
-   * @param {number} options.r border radius (roundness of corners)
+   * @param {number | [number, number, number, number]} options.r border radius (roundness of corners)
    * @param {string} options.borderColor border color
    * @param {string} options.background fill color
    * @param {number} options.borderWidth border width
@@ -37,6 +37,31 @@ export default class Rect extends Element {
     super.draw();
     // Initial rotation about the center of the rectangle
     this.rotate(this.w/2 + this.x, this.h/2 + this.y);
+
+    // 实现自定义四个角的圆角
+    if (Array.isArray(this.r)) {
+      const [topLeft, topRight, bottomRight, bottomLeft] = this.r;
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.x + topLeft, this.y);
+      this.ctx.lineTo(this.x + this.w - topRight, this.y);
+      this.ctx.arcTo(this.x + this.w, this.y, this.x + this.w, this.y + topRight, topRight);
+      this.ctx.lineTo(this.x + this.w, this.y + this.h - bottomRight);
+      this.ctx.arcTo(this.x + this.w, this.y + this.h, this.x + this.w - bottomRight, this.y + this.h, bottomRight);
+      this.ctx.lineTo(this.x + bottomLeft, this.y + this.h);
+      this.ctx.arcTo(this.x, this.y + this.h, this.x, this.y + this.h - bottomLeft, bottomLeft);
+      this.ctx.lineTo(this.x, this.y + topLeft);
+      this.ctx.arcTo(this.x, this.y, this.x + topLeft, this.y, topLeft);
+      this.ctx.closePath();
+      this.ctx.fillStyle = this.background;
+      if (this.borderWidth) {
+        this.ctx.lineWidth = this.borderWidth;
+        this.ctx.strokeStyle = this.borderColor;
+        this.ctx.stroke();
+      }
+      this.ctx.fill();
+      this.rotate(this.w/2 + this.x, this.h/2 + this.y, false);
+      return;
+    }
 
     if (this.w < 2 * this.r) this.r = this.w / 2;
     if (this.h < 2 * this.r) this.r = this.h / 2;
